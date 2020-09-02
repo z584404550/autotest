e28288931f7e59d316bac5099880c038370dc96b
z584404550 <q584404550@sina.com>
2020-09-02 11:35:00 +0800

    autotest

--- apitest/views.py
+++ apitest/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse #加入引用
 # Create your views here.
+def test(request):
+    return HttpResponse("Hello test") #返回HttpResponse响应函数
+def login(request):
+    return render(request,'login.html')
