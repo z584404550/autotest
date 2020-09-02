6fc17079cfe7b1f1e9206b1b23c63eb57f8b821d
z584404550 <q584404550@sina.com>
2020-09-02 13:58:17 +0800

    autotest

--- apitest/views.py
+++ apitest/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse #加入引用
+from django.http import HttpResponse,HttpResponseRedirect #加入引用
+from django.contrib.auth.decorators import login_required
+from django.contrib import  auth
+from django.contrib.auth import authenticate,login
 # Create your views here.
-def test(request):
-    return HttpResponse("Hello test") #返回HttpResponse响应函数
+# def test(request):
+#     return HttpResponse("Hello test") #返回HttpResponse响应函数
 def login(request):
+    if request.POST:
+        username = password =''
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = auth.authenticate(username=username,password=password)
+        if user is not None and user.is_active:
+            auth.login(request,user)
+            request.session['user'] = username
+            response=HttpResponseRedirect('/home/')
+            return  response
+        else:
+            return render(request,'login.html',{'error':'username os password error'})
+        # else:
+        #     context={}
+        #     return render(request,'long.thml',context)
     return render(request,'login.html')
