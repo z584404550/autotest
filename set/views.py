09c50ed6ac29c18b452d1317aebb5b30edc3aa88
z584404550 <q584404550@sina.com>
2020-09-29 15:13:04 +0800

    autotest

--- set/views.py
+++ set/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
 # Create your views here.
+
+
+# 用户管理
+def set_user(request):
+    user_list = User.objects.all()
+    username = request.session.get('user', '')
+    return render(request, "set_user.html", {"user": username, "users": user_list})
