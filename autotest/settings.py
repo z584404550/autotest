e932fb7f350deab7ed28b88ea9674bb28a878965
z584404550 <q584404550@sina.com>
2020-09-30 17:16:55 +0800

    autotest

--- autotest/settings.py
+++ autotest/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,5 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+HERE = os.path.dirname(os.path.abspath(__file__))
+HERE = os.path.join(HERE, '../')
+STATICFILES_DIRS = (os.path.join(HERE, 'static/'),)
 
 X_FRAME_OPTIONS = 'ALLOWALL url'
