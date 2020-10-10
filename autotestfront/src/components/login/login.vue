9928654fc7cf079f0e9ec569094c2791aaef5f8e
z584404550 <q584404550@sina.com>
2020-10-10 17:06:56 +0800

    autotest

--- autotestfront/src/components/login/login.vue
+++ autotestfront/src/components/login/login.vue
@@ -18,26 +18,28 @@
 </template>
 
 <script>
-date() { //页面初数据
-    return {
-        ruleForm: { //存储用户输入数据
-            user: '',
-            password: '',
-        },
-        rules: { // 可添加一些表单规则
+export default {
+    date() { //页面初数据
+        return {
+            ruleForm: { //存储用户输入数据
+                user: '',
+                password: '',
+            },
+            rules: { // 可添加一些表单规则
+            }
         }
-    }
-}，
-methods: {
-    submitFrom() {
-        this.axios.post('url',{...this.ruleForm}).then(res => { // post请求，携带参数为展开运算符=user: '', passwd: '',
-            if(res.code != 0) return false; // 后台根据前端传来的数据返回对应的状态码， 0为成功，继续继续往下执行，非0即失败（-1为用户名或者密码错误，1为空）停止往下执行
-            this.$message({ //提示成功信息
-                type: 'success',
-                $message: '登录成功'
+    },
+    methods: {
+        submitFrom() {
+            this.axios.post('url',{...this.ruleForm}).then(res => { // post请求，携带参数为展开运算符=user: '', passwd: '',
+                if(res.code != 0) return false; // 后台根据前端传来的数据返回对应的状态码， 0为成功，继续继续往下执行，非0即失败（-1为用户名或者密码错误，1为空）停止往下执行
+                this.$message({ //提示成功信息
+                    type: 'success',
+                    $message: '登录成功'
+                })
+                this.$router.push('/home/index') // 成功跳转到首页
             })
-            this.$router.push('/home/index') // 成功跳转到首页
-        })
+        }
     }
 }
 </script>
