3d1f1cc58c9eef5f78bdd82f09744cdc449b7ec0
z584404550 <q584404550@sina.com>
2020-10-22 15:07:18 +0800

    autotest

--- autotestfront/src/components/login/Login.vue
+++ autotestfront/src/components/login/Login.vue
@@ -1,7 +1,42 @@
 <template>
-  <div>
-    测试axios-{{title}}
-    <button @click="requetLogin">登录</button>
+  <div class="main-body">
+    <div class="form">
+      <Icon type="logo-octocat" size="60" />
+      <h1>Sign in to JiaGE</h1>
+    </div>
+    <div class="login">
+      <Form ref="formInline" :model="formInline" :rules="ruleInline">
+        <FormItem prop="user">
+          <div class="prompt">Username or email address</div>
+          <i-Input type="text" v-model="formInline.user" placeholder="Username or Email" clearable>
+            <Icon type="ios-person" slot="prepend" size="16"></Icon>
+          </i-Input>
+        </FormItem>
+        <FormItem prop="password">
+          <div class="prompt" style="float: left">Password</div>
+          <div class="prompt" style="float: right">
+            <a>Forgot password?</a>
+          </div>
+          <i-Input type="password" v-model="formInline.password" placeholder="Password" clearable>
+            <Icon type="ios-lock" slot="prepend" size="16"></Icon>
+          </i-Input>
+        </FormItem>
+        <FormItem>
+          <Button
+            class="btn"
+            type="success"
+            size="large"
+            long
+            :loading="modal_loading"
+            @click="handleSubmit('formInline')"
+          >Sign in</Button>
+        </FormItem>
+      </Form>
+    </div>
+    <p class="register-link">
+      New to JiaGE?
+      <router-link to="/account/register">Create an account.</router-link>
+    </p>
   </div>
 </template>
 
@@ -20,9 +55,6 @@ export default {
   methods: {
     requetLogin () {
       // 获取小结的内容
-      // let date = {
-      //   username: 'admin', password: '123456'
-      // }
       let username = 'admin'
       let password = '123456'
       login(username, password).then((resp) => {
@@ -37,5 +69,41 @@ export default {
 </script>
 
 <style scoped>
-
+.form {
+  text-align: center;
+  width: 400px;
+  margin: 0 auto;
+  margin-top: 30px;
+  margin-bottom: 25px;
+}
+.form h1 {
+  font-size: 24px;
+  font-weight: 100;
+  letter-spacing: -0.5px;
+}
+.login {
+  width: 308px;
+  margin: 0 auto;
+  border: 1px solid #d8dee2;
+  height: 257px;
+  border-radius: 5px;
+  padding: 20px;
+  font-size: 14px;
+  margin-bottom: 15px;
+}
+.prompt {
+  font-size: 13px;
+  font-weight: 600;
+}
+.btn {
+  font-weight: 600;
+}
+.register-link {
+  margin: 0 auto;
+  width: 308px;
+  padding: 15px 20px;
+  text-align: center;
+  border: 1px solid #d8dee2;
+  border-radius: 5px;
+}
 </style>
--- autotestfront/src/main.js
+++ autotestfront/src/main.js
@@ -4,7 +4,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import * as $http from './apis/index' // 从index.js文件引入
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
+Vue.use(ElementUI);
+Vue.use(iView);
 Vue.config.productionTip = false
 Vue.prototype.$http = $http
 
