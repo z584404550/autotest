e278ab1f20b9b8cc3915546922e2766314bca402
z584404550 <q584404550@sina.com>
2020-11-04 17:43:35 +0800

    autotest

--- autotestfront/src/router/index.js
+++ autotestfront/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import router from 'vue-router'
+// import router from 'vue-router'
 // import HelloWorld from '@/components/HelloWorld'
 import Login from "../components/login/Login"
 import Home from "../components/home/Home"
