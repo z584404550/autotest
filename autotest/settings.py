3fcf0de439b4628b41baae715833db56325c0e6f
z584404550 <q584404550@sina.com>
2020-11-26 17:46:56 +0800

    autotest

--- autotest/settings.py
+++ autotest/settings.py
@@ -135,6 +135,7 @@ REST_FRAMEWORK = {
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     )
 }
 
